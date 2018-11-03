3890afc19935a1cfb3eff8bd5a9379858eee168b
minaaoyagi <mina.tazawa@gmail.com>
2018-11-03 11:26:59 +0900

    Add files via upload

---
+++ demo/demo01/js/script.js
@@ -0,0 +1,14 @@
+
+// ハンバーガーメニュー //
+$(function(){
+    $('.menu').on('click', function() {
+        $(this).toggleClass('active');
+        $("#nav").toggleClass('active');
+     })
+   })
+$(function() {
+ $('#nav a').on('click', function() {
+ $('#nav').toggleClass('active');
+ $(".menu").toggleClass('active');
+})
+ });
